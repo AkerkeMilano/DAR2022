bf5b4941a3347819296f3705eea09b2c597ed54e
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2022-03-14 00:55:22 +0600

    Implemented state

--- homework2/src/components/movieItem/MovieItem.tsx
+++ homework2/src/components/movieItem/MovieItem.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Movie } from "../../types";
 type Props = {
     movie: Movie;
 }
 const MovieItem: React.FC<Props> = ({ movie }) => {
+    const [addedToWatchLater, setAddedToWatchLater] = useState(false);
+    const watchLaterClick = () => {
+        setAddedToWatchLater(true);
+        console.log('Clicked', addedToWatchLater);
+    }
     return (
-    <div className="movie-item">
+    <div className="movie-item" style={{ marginBottom: '1rem' }}>
         <img src={movie.image} alt={movie.title} style={{width: '200px'}}/>
         <div>{movie.title}</div>
         <div>{movie.description}</div>
         <div>{movie.duration/60} hr</div>
+        <button onClick={watchLaterClick}>{addedToWatchLater ? 'Added to list' : 'Watch later'}</button>
     </div>
     )
 };
