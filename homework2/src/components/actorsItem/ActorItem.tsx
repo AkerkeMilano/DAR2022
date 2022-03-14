8c10e245ba748e7d9a091229a2b131720793ccf3
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2022-03-14 12:55:33 +0600

    Added button for changing state

--- homework2/src/components/actorsItem/ActorItem.tsx
+++ homework2/src/components/actorsItem/ActorItem.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Actor } from '../../types';
 
 type Props = {
     actor: Actor;
 }
 
-
 const ActorItem: React.FC<Props> = ({ actor} ) => {
+    const [readMoreAboutActor, setReadMoreAboutActor] = useState(false);
+    const readMoreClick = () => {
+        setReadMoreAboutActor(true);
+    }
     return (
         <div className="actor-item" style={{ marginBottom: '1rem' }}>
-        <img src={actor.image} alt={actor.name} style={{width: '200px'}}/>
+        <img src={actor.image} alt={actor.name} style={{width: '400px'}}/>
         <div>Hi! I'm {actor.name}</div>
         <div>{actor.age} years old</div>
         <div>I'm from {actor.country}</div>
         <div>and love {actor.hobby}</div>
+        <button onClick={readMoreClick}>{readMoreAboutActor ? 'Read less' : 'Read more'}</button>
     </div>
     )
 }
